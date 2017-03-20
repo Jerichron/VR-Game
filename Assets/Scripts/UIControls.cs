8210fdc836afb83c1870e1d99a9f5c9d5af0f47d
jamiea483 <jamiea483@hotmail.com>
2017-03-20 17:09:31 -0400

    Timer for the hook works

--- Assets/Scripts/UIControls.cs
+++ Assets/Scripts/UIControls.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIControls : MonoBehaviour {
 
+	float timer = 4;
+	float cooldown = 4;
+	bool startTime = false;
+	public Image hook;
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			startTime = true;
+			cooldown = 0;
+		}
+		if (startTime) {
+			if (cooldown < 4) {
+				cooldown += Time.deltaTime;
+				CoolDown (cooldown);
+			} else {
+				cooldown = 4;
+				startTime = false;
+			}
+
+		}
+	}
+
+	void CoolDown(float time){
+		hook.fillAmount = time / timer;
 	}
 }
