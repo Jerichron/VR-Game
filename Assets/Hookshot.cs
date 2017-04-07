7bad824a1b9a99482cccae9722562accd545f70a
jamiea483 <jamiea483@hotmail.com>
2017-04-07 19:30:21 -0400

    Hook hits the canvas

---
+++ Assets/Hookshot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hookshot : MonoBehaviour {
+
+	public Transform Player;
+	public GameObject Hook;
+	public GameObject HookChild;
+
+
+
+	// Update is called once per frame
+	void Update () {
+		Ray HookRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		Debug.DrawRay (HookRay.origin, HookRay.direction, Color.green);
+		RaycastHit HookHit;
+
+		if (Input.GetMouseButtonDown(0) || Input.GetButtonDown("Fire3"))
+		{
+			HookChild = null;
+			if (Physics.Raycast(HookRay, out HookHit))
+			{
+				Debug.DrawRay (HookRay.origin, HookRay.direction, Color.red);
+				HookChild = Instantiate(Hook, HookHit.point, Quaternion.identity) as GameObject;
+					Player.localPosition = HookHit.point;
+					Debug.Log ("Hook Hit");
+					}
+}
+		if (Input.GetMouseButtonUp(0) || Input.GetButtonUp("Fire3"))
+					{
+						if (HookChild != null)
+							Destroy(HookChild);
+					}}}
---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/UIControls.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIControls : MonoBehaviour {
+
+	float timer = 1;
+	float cooldown = 1;
+	bool startTime = false;
+	public Image hook;
+	public Image healthBar;
+	public PlayerHealth playerHealth;
+
+	public GameManager manager;
+	public Text scoreText;
+
+	void Start(){
+		//GameObject player = GameObject.Find ("Player");
+
+		//display score on start
+		DisplayScore ();
+	}
+
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		//for testing
+		if (Input.GetMouseButtonDown(0) && startTime == false) {
+			startTime = true;
+			cooldown = 0;
+		}
+		if (startTime) {
+			if (cooldown < 1) {
+				cooldown += Time.deltaTime;
+				CoolDown (cooldown);
+			} else {
+				cooldown = 1;
+				startTime = false;
+			}
+		}
+		Health ();
+
+		//Updates the score
+		DisplayScore ();
+	}
+
+	void CoolDown(float time){
+		hook.fillAmount = time / timer;
+	
+	}
+
+	void Health()
+	{
+		if(playerHealth.inSight && playerHealth.alive)
+		{
+			playerHealth.cur_health -= Time.deltaTime;
+			healthBar.fillAmount = playerHealth.cur_health / playerHealth.health;
+		}
+	}
+
+	void DisplayScore(){
+		scoreText.text = ("Score: "+ manager.score);
+	}
+}
+		
