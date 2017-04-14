09afd295e20cb20b7451a393934780b45166b591
jamiea483 <jamiea483@hotmail.com>
2017-04-14 11:31:22 -0400

    Pause menu works with control
    Pause menu works with control and also added music to the main menu.

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PauseMenu : MonoBehaviour {
+	public bool pauseGame = false;
+	private bool showUI = false;
+	public Canvas gameUI;
+	public Canvas pauseUI;
+	public GameObject crossfire;
+	public EventSystem check;
+
+
+
+	void Update(){
+
+		if (Input.GetKeyDown (KeyCode.Joystick1Button7) || Input.GetKeyDown (KeyCode.Escape)) {
+			if (!pauseGame) {
+				pauseGame = true;
+				check.enabled = true;
+			} else {
+				pauseGame = false;
+				check.enabled = false;
+			}
+		}
+		if (pauseGame == true) {
+			Time.timeScale = 0;
+			gameUI.enabled = false;
+			pauseUI.enabled = true;
+
+		} else {
+			Time.timeScale = 1;
+			gameUI.enabled = true;
+			pauseUI.enabled = false;
+		}
+
+	}
+
+	public void Resume(){
+		pauseGame = false;
+	}
+
+	public void Menu(){
+		Application.LoadLevel (0);
+	}
+
+}
